19fb9c5e3325826c49ad7d8f0e6469c5c19e9178
eghkanno <haruto.kanno@e-grant.net>
2018-04-13 18:22:36 +0900

    CSSからJQueryに変更・ドラッグリサイズ実装

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("restyler working");
+$(function(){
+
+const target = $('aside.sidebar');
+const initial_width = 140;
+
+
+target.funcResizeBox({ 
+    minWidth: 10,
+    maxWidth: 1000,
+    mouseRange: 30,
+    isWidthResize:true,
+    isHeightResize:false 
+})
+
+let observer = new MutationObserver(
+    function () {
+        target.trigger('sidebar');
+    }
+);
+
+target.on("sidebar", function(e){
+    let offset = target.width()+1;
+    $('section.main.project article.main').css({'left': offset});
+});
+
+observer.observe(target[0], {
+    attributes: true,
+    characterData: false,
+    childList: true
+});
+
+target.width(initial_width);
+
+});
+
+console.log("restyler working!");
