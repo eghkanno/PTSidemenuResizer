fb20d2ca7b1536f9f74e10802154f986cc95d83c
eghkanno <haruto.kanno@e-grant.net>
2018-05-25 15:44:33 +0900

    関数名変更

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 $(function(){
 
-setTimeout(main ,5000);
+    setTimeout(init ,5000);
 
-function main(){
-const target = $('aside.sidebar');
-const initial_width = 140;
+    function init() {
+
+        const target = $('aside.sidebar');
+        const initial_width = 140;
 
 
-target.funcResizeBox({ 
-    minWidth: 10,
-    maxWidth: 1000,
-    mouseRange: 30,
-    isWidthResize:true,
-    isHeightResize:false 
-})
+        target.funcResizeBox({
+            minWidth: 10,
+            maxWidth: 1000,
+            mouseRange: 30,
+            isWidthResize:true,
+            isHeightResize:false
+        })
 
-let observer = new MutationObserver(
-    function () {
-        target.trigger('sidebar');
+        let observer = new MutationObserver(
+            function () {
+                target.trigger('sidebar');
+            }
+        );
+
+        target.on("sidebar", function(e){
+            let offset = target.width()+1;
+            $('section.main.project article.main').css({'left': offset});
+        });
+
+        observer.observe(target[0], {
+            attributes: true,
+            characterData: false,
+            childList: true
+        });
+
+        target.width(initial_width);
+        console.log("restyler working!");
     }
-);
-
-target.on("sidebar", function(e){
-    let offset = target.width()+1;
-    $('section.main.project article.main').css({'left': offset});
-});
-
-observer.observe(target[0], {
-    attributes: true,
-    characterData: false,
-    childList: true
-});
-
-target.width(initial_width);
-console.log("restyler working!");
-}
 
 });
 
